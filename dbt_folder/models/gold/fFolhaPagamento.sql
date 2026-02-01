9c37fe77461e5c1d7be6d741e6705f498cefa131
Maxwilliam <maxwilliamgomess@gmail.com>
2026-02-01 13:38:24 -0300

    feat: Readme

--- dbt_folder/models/gold/fFolhaPagamento.sql
+++ dbt_folder/models/gold/fFolhaPagamento.sql
@@ -2,7 +2,9 @@ select
     f.funcionarioid,
     f.empresaid,
     f.salario as salario_base,
-    coalesce(ps.valor_plano, 0) as custo_plano_saude,
-    nomeplano_padronizado as nome_plano
+    sum(coalesce(ps.valor_plano, 0)) as custo_plano_saude
 from {{ ref('silver_funcionarios') }} f
 left join {{ ref('silver_PlanoSaude') }} ps on f.funcionarioid = ps.funcionarioid
+group by f.funcionarioid,
+         f.empresaid,
+         f.salario
